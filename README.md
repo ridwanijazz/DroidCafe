93be6bfcbadb2227a506cb31189af34005e8b5e5
ridwanijazz <m.saifurridwani@gmail.com>
2024-10-09 23:39:49 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Result
+<img src="https://github.com/user-attachments/assets/2c394bc2-1a17-4d0b-9276-09706f1f4f71" alt="Image" width="300"/>
+
+<img src="https://github.com/user-attachments/assets/42ca86f5-4d60-42c9-a59f-bee93f66711d" alt="Image" width="500"/>
+
+
---
+++ app/src/main/java/lat/pam/droidcafe/OrderActivity.kt
@@ -0,0 +1,68 @@
+package lat.pam.droidcafe
+
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.RadioButton
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class OrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_order)
+
+        val spinner: Spinner = findViewById(R.id.spinner)
+
+
+        val items = resources.getStringArray(R.array.spinner_city)
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, items)
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinner.adapter = adapter
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun onRadioButtonClicked(view: View) {
+        // Is the button now checked?
+        val checked = (view as RadioButton).isChecked
+        when (view.getId()) {
+            R.id.sameday -> if (checked) // Same day service
+                displayToast(getString(R.string.same_day_messenger_service))
+
+
+            R.id.nextday -> if (checked) // Next day delivery
+                displayToast(getString(R.string.next_day_ground_delivery))
+
+
+            R.id.pickup -> if (checked) // Pick up
+                displayToast(getString(R.string.pick_up))
+
+
+            else -> {}
+        }
+    }
+
+
+    private fun displayToast(message: String) {
+        Toast.makeText(
+            applicationContext, message,
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+}
